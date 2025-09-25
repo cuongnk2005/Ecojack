020ad1098a3671f2a36ac3d87566133421bdde94
kietng.23it@vku.udn.vn <kietnguyengia20005@gmail.com>
2025-09-25 15:33:40 +0700

    add folder

---
+++ app/views/auth/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/public/asset/css/login.css">
+</head>
+
+<body>
+    <div class="login_container">
+        <div class="login-form">
+            <div class="logo-icon">
+                <img src="/public/images/logo_login.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
